2dbefc1a708dbcdf13606b36b1c6c3fe1804ab43
Seba Duarte <sebaduar@hotmail.com>
2020-02-15 00:18:45 -0300

    Movimiento y MovimientoDAO

---
+++ WebApplication1/DAO/MovimientoDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1;
+
+public class MovimientoDAO : DaoBase
+    {
+
+      public static Movimiento AgregarMovimiento(Movimiento mov)
+    {
+		try
+		{
+			int movId = GetNextId(mov);
+
+			if (movId != 0)
+			{
+				mov.Id = movId;
+				CreateEntity(mov);
+				return mov;
+			}
+			else
+			{
+				throw new Exception("No se pudo obtener el siguiente Id de la entidad: Movimiento");
+			}
+
+		}
+		catch (Exception e)
+		{
+
+			throw new Exception("Error en Agregar Movimiento");
+		}
+    }
+
+    }
